7438478ed8868e9b5469aae422ab2d900d997a73
Brunodesa <49911022+Brunodesa@users.noreply.github.com>
2019-10-30 12:21:06 +0000

    Add files via upload

---
+++ JogoNovo/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.square2 = New System.Windows.Forms.PictureBox()
+        Me.square3 = New System.Windows.Forms.PictureBox()
+        Me.square4 = New System.Windows.Forms.PictureBox()
+        Me.square1 = New System.Windows.Forms.PictureBox()
+        Me.player = New System.Windows.Forms.PictureBox()
+        CType(Me.square2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.square3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.square4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.square1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 20
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'square2
+        '
+        Me.square2.Image = Global.JogoNovo.My.Resources.Resources.square
+        Me.square2.Location = New System.Drawing.Point(110, 20)
+        Me.square2.Name = "square2"
+        Me.square2.Size = New System.Drawing.Size(67, 62)
+        Me.square2.TabIndex = 7
+        Me.square2.TabStop = False
+        '
+        'square3
+        '
+        Me.square3.Image = Global.JogoNovo.My.Resources.Resources.square
+        Me.square3.Location = New System.Drawing.Point(208, 20)
+        Me.square3.Name = "square3"
+        Me.square3.Size = New System.Drawing.Size(67, 62)
+        Me.square3.TabIndex = 6
+        Me.square3.TabStop = False
+        '
+        'square4
+        '
+        Me.square4.Image = Global.JogoNovo.My.Resources.Resources.square
+        Me.square4.Location = New System.Drawing.Point(300, 20)
+        Me.square4.Name = "square4"
+        Me.square4.Size = New System.Drawing.Size(67, 62)
+        Me.square4.TabIndex = 4
+        Me.square4.TabStop = False
+        '
+        'square1
+        '
+        Me.square1.Image = Global.JogoNovo.My.Resources.Resources.square
+        Me.square1.Location = New System.Drawing.Point(26, 20)
+        Me.square1.Name = "square1"
+        Me.square1.Size = New System.Drawing.Size(67, 62)
+        Me.square1.TabIndex = 1
+        Me.square1.TabStop = False
+        '
+        'player
+        '
+        Me.player.Image = Global.JogoNovo.My.Resources.Resources.player
+        Me.player.Location = New System.Drawing.Point(209, 662)
+        Me.player.Name = "player"
+        Me.player.Size = New System.Drawing.Size(50, 50)
+        Me.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.player.TabIndex = 0
+        Me.player.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 733)
+        Me.Controls.Add(Me.square2)
+        Me.Controls.Add(Me.square3)
+        Me.Controls.Add(Me.square4)
+        Me.Controls.Add(Me.square1)
+        Me.Controls.Add(Me.player)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.square2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.square3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.square4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.square1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents player As PictureBox
+    Friend WithEvents square1 As PictureBox
+    Friend WithEvents square4 As PictureBox
+    Friend WithEvents square3 As PictureBox
+    Friend WithEvents square2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ JogoNovo/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles player.Click
+
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left
+                Me.player.Left -= 25
+            Case Keys.Up
+                Me.player.Top -= 25
+            Case Keys.Right
+                Me.player.Left += 25
+            Case Keys.Down
+                Me.player.Top += 25
+        End Select
+    End Sub
+
+    Dim largura = 0
+
+    Dim altura1 = 0
+    Dim altura2 = 0
+    Dim altura3 = 0
+    Dim altura4 = 0
+
+    Dim num1 = Int((10 * Rnd()) + 7)
+    Dim num2 = Int((10 * Rnd()) + 7)
+    Dim num3 = Int((10 * Rnd()) + 7)
+    Dim num4 = Int((10 * Rnd()) + 7)
+
+
+    Sub Spawn()
+
+        square1.Location = New Point(26, 20)
+        square2.Location = New Point(100, 20)
+        square3.Location = New Point(208, 20)
+        square4.Location = New Point(259, 20)
+        player.Location = New Point(209, 662)
+
+        largura = 0
+
+        altura1 = 0
+        altura2 = 0
+        altura3 = 0
+        altura4 = 0
+
+        num1 = Int((10 * Rnd()) + 7)
+        num2 = Int((10 * Rnd()) + 7)
+        num3 = Int((10 * Rnd()) + 7)
+        num4 = Int((10 * Rnd()) + 7)
+
+        Timer2.Enabled = True
+
+
+    End Sub
+
+    Sub Inicializa()
+
+        square1.Top += num1
+        altura1 += num1
+
+        If altura1 > 650 Then
+            square1.Location = New Point(26, 12)
+            altura1 = 0
+            num1 = Int((10 * Rnd()) + 7)
+        End If
+
+        square2.Top += num2
+        altura2 += num2
+
+        If altura2 > 650 Then
+            square2.Location = New Point(87, 12)
+            altura2 = 0
+            num2 = Int((10 * Rnd()) + 7)
+        End If
+
+        square3.Top += num3
+        altura3 += num3
+
+        If altura3 > 650 Then
+            square3.Location = New Point(148, 12)
+            altura3 = 0
+            num3 = Int((10 * Rnd()) + 7)
+        End If
+
+        square4.Top += num4
+        altura4 += num4
+
+        If altura4 > 650 Then
+            square4.Location = New Point(209, 12)
+            altura4 = 0
+            num4 = Int((10 * Rnd()) + 7)
+        End If
+    End Sub
+
+    Sub Cheat()
+        Call Inicializa()
+        Call Spawn()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Call Inicializa()
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        For Each falling As PictureBox In {square1, square2, square3, square4}
+            If player.Bounds.IntersectsWith(falling.Bounds) Then
+                num1 = 0
+                num2 = 0
+                num3 = 0
+                num4 = 0
+                Timer2.Enabled = False
+                Dim resposta = MsgBox("Perdeste. Novo jogo?" & vbCrLf, vbYesNo, "Game Over")
+                If resposta = vbYes Then Call Cheat()
+                If resposta = vbNo Then Me.Close()
+            End If
+        Next
+    End Sub
+End Class
